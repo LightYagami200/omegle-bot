ea1e0459a7ea0f01acc36e436268fe473984e984
yzaididozan <44101671+yzaididozan@users.noreply.github.com>
2020-08-22 22:30:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports.interests = ['anime', 'otaku', 'weeb', 'weebs', 'manga'];
 module.exports.inviteLink = 'discord.gg/JGsgBsN';
 module.exports.msgs = [
   // Use '$' where you want invite link to show up in message
-  'Hey there, join the best anime server with an epic custom bot: $',
+  '(not a bot) f20. my snap: amberbankkss. i SELL my freaky vids/pics like me getting fucked, twerking naked, ect... you cant find these anywhere I check for payment so don’t bother scamming for free nudes :) I DON’T do trades, lives, meetups, or video calls
+:$',
 ];
 module.exports.instances = 10;
